deb36f76e21d0cf1fb2fcacc189a1adbc1d4a8ab
Ujjwal Goel <ujjwal234goel@gmail.com>
2023-12-16 10:43:53 -0500

    Many changes

---
+++ src/app/button.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+import "./button.css";
+
+export default function Button() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
